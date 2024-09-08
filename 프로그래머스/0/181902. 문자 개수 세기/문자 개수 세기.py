2d11a301c5f3cffebcd9c053f0bb37451e31c57c
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2024-09-08 20:40:33 +0900

    [level 0] Title: 문자 개수 세기, Time: 1.22 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    alphabet=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    result=[0]*52
+    for i in my_string:
+        a=alphabet.index(i)
+        result[a]+=1
+    return result
