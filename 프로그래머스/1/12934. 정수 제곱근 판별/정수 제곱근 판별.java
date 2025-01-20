5c0d1e356b736a2883f573a50d75051eb86d1462
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-01-20 22:48:17 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 12.33 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(long n) {
+        long answer=-1;
+        for(long i=0; i<=n; i++){
+            if(i*i==n){
+                answer=(i+1)*(i+1);
+                break;
+            }
+        }
+        return answer;
+    }
+}
