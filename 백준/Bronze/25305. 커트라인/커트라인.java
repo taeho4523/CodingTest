714643e7eff40c0e054132db2bcf3fc7683acce5
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-09-03 18:43:04 +0900

    [Bronze II] Title: 커트라인, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st= new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++){
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		bw.write(String.valueOf(arr[arr.length-k]));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
