e129b3c2780b9ffb4f8077ada5abdcea2c02551d
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-09-17 16:56:45 +0900

    [Silver V] Title: K번째 수, Time: 1920 ms, Memory: 379072 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        bw.write(String.valueOf(arr[K-1]));
+        bw.flush();
+    }
+}
