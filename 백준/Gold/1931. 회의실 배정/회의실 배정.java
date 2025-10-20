e94608f3e74e9713bf81de59ac52b553f2f46665
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-10-20 20:16:42 +0900

    [Gold V] Title: 회의실 배정, Time: 1128 ms, Memory: 175148 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,39 +1,44 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int N = s.nextInt();
-        int[][] timetable = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            timetable[i][0] = s.nextInt();
-            timetable[i][1] = s.nextInt();
+        Scanner n = new Scanner(System.in);
+        int N = n.nextInt(); //회의 갯수 입력받기
+        ArrayList<Time> t = new ArrayList<>();
+        for(int i=0; i<N; i++){
+            int start=n.nextInt();                //시작시간
+            int end=n.nextInt();                  //종료시간 입력해주기
+            t.add(new Time(start, end));
         }
-
-        //timetable 정렬 다시 정의
-        Arrays.sort(timetable, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) {
-                    return o1[0] - o2[0];
-                }
-
-                return o1[1] - o2[1];
+        t.sort((o1, o2) -> {            //그냥t.sort로 종료시간으로만 정렬했더니 메모리초과 발생. 종료시간이 같으면 빠른시작시간을 우선정렬
+            if (o1.end == o2.end) {
+                return o1.start - o2.start; // 종료시간이 같으면 시작시간이 빠른 회의를 우선으로 선택
+            } else {
+                return o1.end - o2.end;
             }
         });
-
-        int result = 0;
-        int end_time = 0;
-        for (int i = 0; i < N; i++) {
-            if (timetable[i][0] >= end_time) {
-                end_time = timetable[i][1];
-                result++;
+        Time Time;
+        int endTime =0;
+        int count =0;
+        for(Time time : t) {
+            if(time.start >= endTime) {
+                endTime = time.end;
+                count++;
             }
         }
-        System.out.println(result);
+        System.out.println(count);
 
+    }
+    static class Time {
+        int start;
+        int end;
+        //메모리초과 해결해보려고 tostring 지워봄..
 
+        public Time(int start, int end){
+            this.start=start;
+            this.end=end;
+        }
     }
 }
