1b1ded4a521f58bce8782485ef65b9467b56bc77
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-11-25 01:04:23 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n=Integer.parseInt(br.readLine());
+        dp=new int[n+1];
+        for(int i=1; i<=n; i++){
+            dp[i]=i;
+            for(int j=1; j*j<=i; j++){
+                dp[i]=Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
