87d23e8868b3a7c16d702bb2de5de327ccd8ed59
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-11-27 20:16:27 +0900

    [Silver V] Title: 다리 놓기, Time: 120 ms, Memory: 14476 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+            long result=1;
+            for(int j=1;j<=a;j++){
+                result = result * (long)(b - j + 1) / j;
+            }
+            System.out.println(result);
+        }
+    }
+}
