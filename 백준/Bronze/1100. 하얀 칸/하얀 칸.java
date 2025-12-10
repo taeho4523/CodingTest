f7587ee8cde2249fb4aaf67e95ef0cee59d5a7a6
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-12-10 20:05:28 +0900

    [Bronze II] Title: 하얀 칸, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                if (s.charAt(j) == 'F' && (i+j)%2 == 0) count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
