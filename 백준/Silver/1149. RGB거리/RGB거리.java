269059b0dc94b93a7ff39c8cc0b051d6a1694e77
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2025-12-15 17:05:43 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14644 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static int[][] arr,dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        int home=Integer.parseInt(br.readLine());
+        arr=new int[home][3];
+        dp=new int[home][3];
+        for(int i=0;i<home;i++){
+            StringTokenizer st =new StringTokenizer(br.readLine());
+            for(int j=0;j<3;j++){
+                arr[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0]=arr[0][0];
+        dp[0][1]=arr[0][1];
+        dp[0][2]=arr[0][2];
+
+        for(int i=1;i<home;i++){
+            dp[i][0]=Math.min(dp[i-1][1],dp[i-1][2])+arr[i][0];
+            dp[i][1]=Math.min(dp[i-1][0],dp[i-1][2])+arr[i][1];
+            dp[i][2]=Math.min(dp[i-1][0],dp[i-1][1])+arr[i][2];
+        }
+        System.out.println(Math.min(dp[home-1][2],Math.min(dp[home-1][0],dp[home-1][1])));
+
+    }
+}
